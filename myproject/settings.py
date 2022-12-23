d29d9ec12a298524eae15a887b7ac0e6bf75f61c
akhilpkb <79098690+akhilpkb@users.noreply.github.com>
2022-12-23 18:30:44 +0530

    database change

--- myproject/settings.py
+++ myproject/settings.py
@@ -75,16 +75,22 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'sportsapp',
+#         'USER': 'root',
+#         'PASSWORD': '',
+#         'PORT':'3306'
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'sportsapp',
-        'USER': 'root',
-        'PASSWORD': '',
-        'PORT':'3306'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
